cf61f7c3b3db273b7784d335566d00c8c227d150
farhaghallab3 <farhagalab74@gmail.com>
2024-12-30 02:08:31 +0200

    update test

--- frontend/cypress/e2e/home.cy.js
+++ frontend/cypress/e2e/home.cy.js
@@ -32,7 +32,7 @@ describe('Home Page Tests', () => {
 
  it('Should open the menu about the website', () => {
     cy.visit('http://localhost:3000');
-    cy.contains('☰|☰').click(); // Open the menu
+    cy.contains('☰').click(); // Open the menu
     cy.contains('Über diese Website').should('be.visible'); // Check menu content
     cy.contains('schließen').click(); // Close the modal
     cy.contains('Dashboard für Finanzinstrumente').should('be.visible'); // Ensure the home page is still visible

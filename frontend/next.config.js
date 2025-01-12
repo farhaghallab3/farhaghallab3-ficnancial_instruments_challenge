83fc81c6227e61ae79a927687eb8f582ad809558
farhaghallab3 <132375082+farhaghallab3@users.noreply.github.com>
2025-01-12 02:58:36 +0200

    pop

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   pageExtensions: ['js', 'jsx'],
+  trailingSlash: true, // Optional, depending on your deployment
+  output: 'standalone', // Optional, depending on your deployment
   
 };

9bd01e532aa2baa151c6e9b147ccee0935f184ce
farhaghallab3 <132375082+farhaghallab3@users.noreply.github.com>
2025-01-12 16:46:13 +0200

    2

--- frontend/cypress/integration/sample-test.js
+++
@@ -1,6 +0,0 @@
-describe('Home Page', () => {
-  it('should load the home page', () => {
-    cy.visit(`${import.meta.env.VITE_REACT_APP_BASE_URL}`);
-    cy.contains('Financial Instruments Dashboard').should('be.visible');
-  });
-});
